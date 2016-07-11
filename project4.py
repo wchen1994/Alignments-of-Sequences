b79141b8c65d4dc08a15e46a7adec58d2d9371c2
wchen1994 <weigan.chen@outlook.com>
2016-07-11 15:38:27 +1000

    Add function compute_alignment_matrix to the main file and test function for it

--- project4.py
+++ project4.py
@@ -26,3 +26,43 @@ def build_scoring_matrix(alphabet, diag_score, off_diag_score, dash_score):
             else:
                 dict1[letter1][letter2] = off_diag_score
     return dict1
+
+def compute_alignment_matrix(seq_x, seq_y, scoring_matrix, global_flag):
+    """
+    compute the score for every alignment of seq_x and seq_y according to
+    the scoring matrix.
+    if global_flag == True:
+        can score negative number. It is useful for global alignment
+    if global_flag == False:
+        every socre less than 0 will become 0. It is useful for local
+        alignment
+    """
+    len_seq_x = len(seq_x)
+    len_seq_y = len(seq_y)
+    lowest_score = float('-inf') if global_flag else 0
+
+    # initialize matrix[0][0] = 0
+    align_matrix = [[float('nan') for dummy_col in range(len_seq_y + 1)] 
+                            for dummy_row in range(len_seq_x +1)]
+    align_matrix[0][0] = 0
+
+    # initialize alignment of '-' and seq_y, or matrix first row
+    for col in range(1, len_seq_y + 1):
+        score = align_matrix[0][col-1] + scoring_matrix[seq_y[col-1]]['-']
+        align_matrix[0][col] = max(score, lowest_score)
+
+    # initialize alignment of seq_x and '-', or matrix first column
+    for row in range(1, len_seq_x + 1):
+        score = align_matrix[row-1][0] +  scoring_matrix['-'][seq_x[row-1]]
+        align_matrix[row][0] = max(score, lowest_score)
+
+    # fill in the rest according to the previous calculation
+    for row in range(1, len_seq_x + 1):
+        for col in range(1, len_seq_y + 1):
+            score_list = [lowest_score]
+            score_list.append( align_matrix[row-1][col-1] + scoring_matrix[seq_x[row-1]][seq_y[col-1]] )
+            score_list.append( align_matrix[row][col-1] + scoring_matrix['-'][seq_y[col-1]] )
+            score_list.append( align_matrix[row-1][col] + scoring_matrix[seq_x[row-1]]['-'] )
+            align_matrix[row][col] = max(score_list)
+
+    return align_matrix
--- test1.py
+++ test1.py
@@ -8,4 +8,15 @@ def test_build_scoring_matrix():
     assert matrix['A']['-'] == -6
     assert matrix['-']['-'] == -6
 
+def test_compute_alignment_matrix():
+    alphabet = {'A', 'T', 'C', 'G'}
+    score_matrix = project4.build_scoring_matrix(alphabet, 6, 2, -4)
+    align_matrix = project4.compute_alignment_matrix(['A'], ['A'], score_matrix, False)
+    assert align_matrix == [[0,0],[0,6]]
+
+def print_matrix(matrix):
+    for row in matrix:
+        print row
+
 test_build_scoring_matrix()
+test_compute_alignment_matrix()

53c5bdbaf0a77a4206d6225a606dbde49a06a993
wchen1994 <weigan.chen@outlook.com>
2016-07-17 23:10:20 +1000

    More tide app4.py and new url_file_read.py to replace alg_application4_provided.py

--- alignSeq/app4.py
+++ alignSeq/app4.py
@@ -1,99 +1,121 @@
-import alg_application4_provided as provided
+import url_file_read as provided
 import math
 import random
 import project4
 import matplotlib.pyplot as plt
 
-#protein_human = provided.read_protein(provided.HUMAN_EYELESS_URL)
-#protein_fly = provided.read_protein(provided.FRUITFLY_EYELESS_URL)
-#scoring_matrix = provided.read_scoring_matrix(provided.PAM50_URL)
+protein_human = provided.read_protein(provided.HUMAN_EYELESS_URL)
+protein_fly = provided.read_protein(provided.FRUITFLY_EYELESS_URL)
+scoring_matrix = provided.read_scoring_matrix(provided.PAM50_URL)
 
-#print "Q1"
-#
-#alignment_matrix = project4.compute_alignment_matrix(protein_human, protein_fly, scoring_matrix, False)
-#result = project4.compute_local_alignment(protein_human, protein_fly, scoring_matrix, alignment_matrix)
-#
-#print result[0]
-#print result[1]
-#print result[2]
-#
-#print "Q2"
-#sequence_human = result[1].replace('-', '')
-#sequence_fly = result[2].replace('-', '')
-#
-#protein_consensus = read_protein(CONSENSUS_PAX_URL)
-#alignment_matrix = project4.compute_alignment_matrix(sequence_human, protein_consensus, scoring_matrix, True)
-#result = project4.compute_global_alignment(sequence_human, protein_consensus, scoring_matrix, alignment_matrix)
-#
-#mark = 0
-#for idx in range(len(result[1])):
-#    if result[1][idx] == result[2][idx]:
-#        mark += 1
-#print mark / float(len(result[1]))
-#
-#protein_consensus = read_protein(CONSENSUS_PAX_URL)
-#alignment_matrix = project4.compute_alignment_matrix(sequence_fly, protein_consensus, scoring_matrix, True)
-#result = project4.compute_global_alignment(sequence_fly, protein_consensus, scoring_matrix, alignment_matrix)
-#mark = 0
-#for idx in range(len(result[1])):
-#    if result[1][idx] == result[2][idx]:
-#        mark += 1
-#print mark / float(len(result[1]))
+"""
+caluclate the score of alignment of human and fruit fly eyeless protein
+"""
+def align_human_fly_protein():
+    alignment_matrix = project4.compute_alignment_matrix(protein_human, protein_fly, scoring_matrix, False)
+    result = project4.compute_local_alignment(protein_human, protein_fly, scoring_matrix, alignment_matrix)
 
-# ploting histogram
-#def save_dict(dictionary):
-#    dict_file = open("distribution.csv", "w")
-#    for key, value in dictionary.items():
-#        dict_file.write(str(key)+","+str(value)+"\n")
-#    dict_file.close()
-#
-#def read_dict(fname):
-#    dict_file = open(fname, "r")
-#    dictionary = {}
-#    for line in dict_file:
-#        line = line.strip()
-#        key, value = line.split(",")
-#        dictionary[int(key)] = int(value)
-#    return dictionary
-#
-#def generate_null_distribution(seq_x, seq_y, scoring_matrix, num_trials):
-#    distribution = {}
-#    bar = progressbar.ProgressBar(max_value=1000)
-#    for progress in range(num_trials):
-#        bar.update(progress)
-#        rand_y = list(seq_y)
-#        random.shuffle(rand_y)
-#        alignment_matrix = project4.compute_alignment_matrix(seq_x, rand_y, scoring_matrix, False)
-#        score = project4.compute_local_alignment(seq_x, rand_y, scoring_matrix, alignment_matrix)[0]
-#        distribution[score] = distribution.get(score,0) + 1
-#    save_dict(distribution)
-#    return distribution
-#
-#READ = True
-#if READ:
-#    dist =read_dict("distribution.csv")
-#else:
-#    dist = generate_null_distribution(protein_human, protein_fly, scoring_matrix, 1000)
-#
-#x = dist.keys()
-#y = dist.values()
-#
-#y_normal = [idx/1000.0 for idx in y]
-#plt.bar(x, y_normal)
-#plt.title("Null distribution using 1000 trials")
-#plt.xlabel("Scores")
-#plt.ylabel("Fraction of trials")
-#plt.show()
+    return result
 
-# Calculate mean and standard deviation
-#score_list = []
-#for score, appearance in dist.items():
-#    score_list += [score] * appearance
-#mean = sum(score_list)/float(len(score_list))
-#stdv = math.sqrt(sum([(value - mean) ** 2 for value in score_list])/float(len(score_list)))
-#print mean, stdv
+#score, human_protein_aligned, fly_protein_aligned = align_human_fly_protein()
+#print score
 
-# Spelling Check
+"""
+calculate the ratio of similar the human of fly compare to consensus protein
+"""
+def calculate_similar_ratio():
+    result = align_human_fly_protein()
+    sequence_human = result[1].replace('-', '')
+    sequence_fly = result[2].replace('-', '')
+    
+    protein_consensus = provided.read_protein(provided.CONSENSUS_PAX_URL)
+    alignment_matrix = project4.compute_alignment_matrix(sequence_human, protein_consensus, scoring_matrix, True)
+    result = project4.compute_global_alignment(sequence_human, protein_consensus, scoring_matrix, alignment_matrix)
+    
+    mark = 0
+    for idx in range(len(result[1])):
+        if result[1][idx] == result[2][idx]:
+            mark += 1
+    print mark / float(len(result[1]))
+    
+    protein_consensus = provided.read_protein(provided.CONSENSUS_PAX_URL)
+    alignment_matrix = project4.compute_alignment_matrix(sequence_fly, protein_consensus, scoring_matrix, True)
+    result = project4.compute_global_alignment(sequence_fly, protein_consensus, scoring_matrix, alignment_matrix)
+    mark = 0
+    for idx in range(len(result[1])):
+        if result[1][idx] == result[2][idx]:
+            mark += 1
+    print mark / float(len(result[1]))
+
+#calculate_diff_ratio()
+
+"""
+ploting histogram about score of alignment to the disorder sequence
+"""
+def save_dict(dictionary):
+    dict_file = open("distribution.csv", "w")
+    for key, value in dictionary.items():
+        dict_file.write(str(key)+","+str(value)+"\n")
+    dict_file.close()
+
+def read_dict(fname):
+    dict_file = open(fname, "r")
+    dictionary = {}
+    for line in dict_file:
+        line = line.strip()
+        key, value = line.split(",")
+        dictionary[int(key)] = int(value)
+    return dictionary
+
+def generate_null_distribution(seq_x, seq_y, scoring_matrix, num_trials):
+    distribution = {}
+    bar = progressbar.ProgressBar(max_value=1000)
+    for progress in range(num_trials):
+        bar.update(progress)
+        rand_y = list(seq_y)
+        random.shuffle(rand_y)
+        alignment_matrix = project4.compute_alignment_matrix(seq_x, rand_y, scoring_matrix, False)
+        score = project4.compute_local_alignment(seq_x, rand_y, scoring_matrix, alignment_matrix)[0]
+        distribution[score] = distribution.get(score,0) + 1
+    save_dict(distribution)
+    return distribution
+
+def plot_histogram():
+    READ = True
+    if READ:
+        dist =read_dict("distribution.csv")
+    else:
+        dist = generate_null_distribution(protein_human, protein_fly, scoring_matrix, 1000)
+
+    x = dist.keys()
+    y = dist.values()
+
+    y_normal = [idx/1000.0 for idx in y]
+    plt.bar(x, y_normal)
+    plt.title("Null distribution using 1000 trials")
+    plt.xlabel("Scores")
+    plt.ylabel("Fraction of trials")
+    plt.show()
+
+#plot_histogram()
+
+"""
+calculate the mean and stdard deviation of the distribution of score over disorder sequence
+"""
+def cal_mean_stdv():
+    score_list = []
+    dist =read_dict("distribution.csv")
+    for score, appearance in dist.items():
+        score_list += [score] * appearance
+    mean = sum(score_list)/float(len(score_list))
+    stdv = math.sqrt(sum([(value - mean) ** 2 for value in score_list])/float(len(score_list)))
+    return mean, stdv
+
+#print cal_mean_stdv()
+
+"""
+Spelling Checking
+"""
 word_list = provided.read_words(provided.WORD_LIST_URL)
 
 def check_spelling(checked_word, dist, word_list):
@@ -115,18 +137,18 @@ def fast_check_spelling(checked_word, dist, word_list):
     word_set = set(word_list)
     align_set = set([])
     for word in word_set:
-        if compute_valid(checked_word, word, dist):
+        if check_valid(checked_word, word, dist):
             align_set.add(word)
     return align_set
 
-def compute_valid(checked_word, word, dist):
+def check_valid(checked_word, word, dist):
     if dist < 0:
         return False
     elif checked_word and word:
         if checked_word[0] == word[0]:
-            return compute_valid(checked_word[1:], word[1:], dist)
+            return check_valid(checked_word[1:], word[1:], dist)
         else:
-            return compute_valid(checked_word, word[1:], dist - 1) or compute_valid(checked_word[1:], word, dist - 1) or compute_valid(checked_word[1:], word[1:], dist - 1)
+            return check_valid(checked_word, word[1:], dist - 1) or check_valid(checked_word[1:], word, dist - 1) or check_valid(checked_word[1:], word[1:], dist - 1)
     elif checked_word:
         if dist - len(checked_word) < 0:
             return False
@@ -140,11 +162,6 @@ def compute_valid(checked_word, word, dist):
     else:
         return True
 
-#print compute_valid("firefly", "direly", 2)
-#print compute_valid("direly", "firefly", 2)
-#
-#print compute_valid("firefly", "finely", 2)
-#print compute_valid("finely", "firefly", 2)
-
-print fast_check_spelling("humble", 1, word_list)
+#print fast_check_spelling("humble", 1, word_list)
+#print fast_check_spelling("firefly", 2, word_list)
 
---
+++ test/test2.py
@@ -0,0 +1,12 @@
+import alignSeq.url_file_read as url_file_read
+
+# URLs for data files
+PAM50_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_PAM50.txt"
+HUMAN_EYELESS_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_HumanEyelessProtein.txt"
+FRUITFLY_EYELESS_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_FruitflyEyelessProtein.txt"
+CONSENSUS_PAX_URL = "http://storage.googleapis.com/codeskulptor-alg/alg_ConsensusPAXDomain.txt"
+WORD_LIST_URL = "http://storage.googleapis.com/codeskulptor-assets/assets_scrabble_words3.txt"
+
+def test_get_url_filename():
+    assert url_file_read.get_url_filename(PAM50_URL) == "alg_PAM50.txt"
+
